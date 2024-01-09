28417255e833025f603f20071b7f9ba534d4da24
Steve Schwab <schwabs@umich.edu>
2024-01-09 14:17:53 -0800

    refactor

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,3 @@
-// ❗ IMPORTANT
-// The ✨ tasks found inside this component are not in order.
-// Check the README for the appropriate sequence to follow.
 import React, { useState, useEffect } from 'react'
 
 let id = 0
@@ -19,77 +16,54 @@ I enjoy bringing creativity and aesthetics to the digital world."
   },
 ]
 
-const initialFormValues = {
+const initialFormValues = () => ({
   fname: "", 
   lname: "", 
   bio: ""
-}
+});
 
 export default function App() {
-  const [members, setMembers] = useState(teamMembers)
-  const [editing, setEditing] = useState(null)
-  // ✨ Create a third state to track the values of the inputs
-  const [formValues, setFormValues] = useState(initialFormValues)
+  const [members, setMembers] = useState(teamMembers);
+  const [editing, setEditing] = useState(null);
+  const [formValues, setFormValues] = useState(initialFormValues());
 
   useEffect(() => {
-    // ✨ If the `editing` state changes from null to the number 2 (for example)
-    // this means we need to populate the inputs of the form
-    // with the data belonging to the member with id 2.
-    // On the other hand, if the `editing` state changes back to null
-    // then we need to reset the form back to empty values
     if (editing) {
-      setFormValues(members.find(member => member.id === editing));
+      const { fname, lname, bio } = members.find(mem => mem.id == editing);
+      setFormValues({ fname, lname, bio })
     } else {
-      setFormValues(initialFormValues);
+      setFormValues(initialFormValues());
     }
   }, [editing])
 
   const onChange = evt => {
-    // ✨ This is the change handler for your text inputs and your textarea.
-    // You can check `evt.target.id` to know which input changed
-    // and then you can use `evt.target.value` to update the state of the form
-    setFormValues({ ...formValues, [evt.target.id]: evt.target.value })
+    const { id, value } = evt.target;
+    setFormValues(prevValues => ({ ...prevValues, [id]: value }))
   }
   const edit = id => {
-    // ✨ Put this function inside a click handler for the <button>Edit</button>.
-    // It should change the value of `editing` state to be the id of the member
-    // whose Edit button was clicked
     setEditing(id);
   }
   const submitNewMember = () => {
-    // This takes the values of the form and constructs a new member object,
-    // which is then concatenated at the end of the `members` state
-    const newMember = {
-      fname: formValues.fname.trim(),
-      lname: formValues.lname.trim(),
-      bio: formValues.bio
-    }
-    
+    const { fname, lname, bio } = formValues;
+    const newMember = { fname, lname, bio, id: getId() };
     setMembers([ ...members, newMember]);
   }
   const editExistingMember = () => {
-    // ✨ This takes the values of the form and replaces the data of the
-    // member in the `members` state whose id matches the `editing` state
-    const memIdx = members.findIndex(member => member.id === editing);
-    const membersCopy = [ ...members ];
-    membersCopy[memIdx].fname = formValues.fname.trim();
-    membersCopy[memIdx].lname = formValues.lname.trim();
-    membersCopy[memIdx].bio = formValues.bio;
-    setMembers(membersCopy);
+    setMembers(prevMembers => prevMembers.map(mem => {
+      if (mem.id == editing) {
+        return { ...mem, ...formValues }
+      }
+      return mem;
+    }))
   }
   const onSubmit = evt => {
-    // ✨ This is the submit handler for your form element.
-    // It will call either `submitNewMember` or `editExistingMember`
-    // depending on whether the `editing` state is null or has an id in it.
-    // Don't allow the page to reload! Prevent the default behavior
-    // and clean up the form after submitting
     evt.preventDefault();
     if (editing) {
       editExistingMember();
     } else {
       submitNewMember();
     }
-    setFormValues(initialFormValues);
+    setFormValues(initialFormValues());
     setEditing(null);
   }
   return (
@@ -115,17 +89,17 @@ export default function App() {
         <form onSubmit={onSubmit}>
           <div>
             <label htmlFor="fname">First Name </label>
-            <input value={formValues.fname} id="fname" type="text" placeholder="Type First Name" onChange={onChange}/>
+          <input onChange={onChange} value={formValues.fname} id="fname" type="text" placeholder="Type First Name"/>
           </div>
 
           <div>
             <label htmlFor="lname">Last Name </label>
-            <input value={formValues.lname} id="lname" type="text" placeholder="Type Last Name" onChange={onChange}/>
+            <input onChange={onChange} value={formValues.lname} id="lname" type="text" placeholder="Type Last Name"/>
           </div>
 
           <div>
             <label htmlFor="bio">Bio </label>
-            <textarea value={formValues.bio} id="bio" placeholder="Type Bio" onChange={onChange}/>
+            <textarea onChange={onChange} value={formValues.bio} id="bio" placeholder="Type Bio"/>
           </div>
 
           <div>
